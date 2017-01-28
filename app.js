beeeec671879e5a27590ee8f0060782ffcee2e54
sudeep267 <sudeep.reddy89@gmail.com>
2017-01-28 20:29:33 +0530

    First commit

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-var express = require('express');
+var express  = require('express');
+var connect = require('connect');
 var app = express();
 var port = process.env.PORT || 8080;
+//var port = 3000;
 
-app.get('/', function (req, res) {
-	var msg = req.body.msg;
-  res.send('Hello World! This is Sudeep...Message from app: '+ msg)
-})
+/*app.get('/', function (req, res) {
+	var msg = req.body;
+  res.send('Hello World! This is Sudeep...Message from app: '+msg)
+})*/
+
+// Routes
+ 
+require('./routes/routes.js')(app);
 
 app.listen(port, function () {
   console.log('Example app listening on port'+ port)
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-var requests = require('config/requests');
+//var requests = require('config/requests');
 var request = require('request');
  
  
@@ -12,53 +12,11 @@ module.exports = function(app) {
     });
  
  
-    app.post('/login',function(req,res){
-        var name = req.body.name;
-            var mobno = req.body.mobno;
-            var reg_id = req.body.reg_id;
+    app.post('/sendm',function(req,res){
+        var msg = req.body.msg;
+		res.send('Hello World! This is Sudeep...Message from app: '+msg)
  
-        requests.login(name,mobno,reg_id,function (found) {
-            console.log(found);
-			console.log("Logged in with :"+name+" "+mobno+" "+reg_id);
-            res.json(found);
-    });
-    });
- 
-    app.post('/send',function(req,res){
-        var fromu = req.body.from;
-        var fromn = req.body.fromn;
-            var to = req.body.to;
-            var msg = req.body.msg;
- 
- 
-        requests.send(fromn,fromu,to,msg,function (found) {
-            console.log(found);
-            res.json(found);
-			console.log("Sending from :"+fromu+" "+fromn+" To "+to+" Msg "+msg);
-    });
-    });
- 
-    app.post('/getuser',function(req,res){
-        var mobno = req.body.mobno;
- 
-        requests.getuser(mobno,function (found) {
-            console.log(found);
-            res.json(found);
-			console.log("Getting User "+mobno);
-    });
-    });
- 
-    app.post('/logout',function(req,res){
-        var mobno = req.body.mobno;
- 
- 
-        requests.removeuser(mobno,function (found) {
-            console.log(found);
-            res.json(found);
-			console.log("Logging off "+mobno);
-    });
-    });
- 
+    }); 
  
 };
 	
